734570a407d831bcf9c4a324cf5c9a4ffcbb9b76
Juliane Blier <blier.juliane@gmail.com>
2016-10-06 12:22:14 +0200

    Structure du fonctionnement du programme et test data-attribute

---
+++ app.js
@@ -0,0 +1,29 @@
+/*
+	- Si click button alors case noire le plus vers le bas de la colonne se colore})
+		- fonction vérifier la couleur noire
+		- fonction remonter à la case suivante
+	- Système de jeu à 2 joueurs avec currentPlayer = 1 ou 2 
+	- Détection 4 case à la suite de la même couleur 
+*/
+
+var currentPlayer = 1;
+var currentColumn;
+var currentRow;
+var currentColor;
+	
+	//Fonction qui renvoie la couleur de la case vérifiée
+function verifyColor(row, column){
+	
+	$('').css('background-color');
+	currentColor = 
+	return currentColor;
+}
+	// Fonction qui remonte d'une case vers le haut
+function goUp(row, column){
+
+}
+
+
+$('button').on('click', function(){
+
+});
