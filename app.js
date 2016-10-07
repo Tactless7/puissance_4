f0a3e9099a85209731a00a9d99f55fb56b2ce283
Juliane Blier <blier.juliane@gmail.com>
2016-10-07 09:09:33 +0200

    Bug fixed and coloration
    First column bug fixed and working coloration

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 /*
-	- Si click button alors case noire le plus vers le bas de la colonne se colore})
+	- Si click button alors case noire le plus vers le bas de la colonne se colore)
 		- fonction vérifier la couleur noire
 		- fonction remonter à la case suivante
 	- Système de jeu à 2 joueurs avec currentPlayer = 1 ou 2 
@@ -8,22 +8,63 @@
 
 var currentPlayer = 1;
 var currentColumn;
-var currentRow;
+var currentRow = 6;
 var currentColor;
+var blackOrElse;
+
+var black = 'rgb(0, 0, 0)';
+var yellow = 'rgb(255, 255, 0)';
+var red = 'rgb(255, 0, 0)';
 	
-	//Fonction qui renvoie la couleur de la case vérifiée
+	//Fonction qui renvoie un booléen true si noire, false si colorée
 function verifyColor(row, column){
-	
-	$('').css('background-color');
-	currentColor = 
-	return currentColor;
+	currentColor = $('td[data-row='+row+'][data-column='+column+']').css('background-color');
+	console.log('currentColor: ' + currentColor);
+	if (currentColor === black) {
+		console.log('Test black')
+		return blackOrElse = true;
+	}
+	else {
+		return blackOrElse = false;
+	}
 }
+
 	// Fonction qui remonte d'une case vers le haut
-function goUp(row, column){
+function goUp(row){
+	currentRow = row - 1;
+}
+
+	// Fonction qui teste la couleur de la case et la remplit par la bonne couleur si nécessaire
+function caseColor(row, column){
+	if(blackOrElse === true && currentPlayer === 1){
+		$('td[data-row='+row+'][data-column='+column+']').css('background-color', red);
+		console.log('black donc red');
+		currentPlayer = 2;
+		currentRow = 6;
+	}
+	else if(blackOrElse === true && currentPlayer === 2){
+		$('td[data-row='+row+'][data-column='+column+']').css('background-color', yellow);
+		console.log('black donc jaune')
+		currentPlayer = 1;
+		currentRow = 6;
+	}
+	if(blackOrElse === false){
+		goUp(currentRow);
+		console.log(row);
+	}
+}
+
+function caseColorV2(row, column){
 
 }
 
 
 $('button').on('click', function(){
+	currentColumn = parseInt($(this).data('column'));
+	console.log(currentRow + ', ' + currentColumn);
 
+	verifyColor(currentRow, currentColumn);
+	caseColor(currentRow, currentColumn);
+	console.log('val currow' + currentRow);
+	//debugger
 });
