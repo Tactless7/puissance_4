2b1bcbd027f78daebbedea7f6c9cfd7200353082
Juliane Blier <blier.juliane@gmail.com>
2016-10-07 10:12:57 +0200

    Puissance 4 V1
    Working Puissance 4

--- app.js
+++ app.js
@@ -1,11 +1,3 @@
-/*
-	- Si click button alors case noire le plus vers le bas de la colonne se colore)
-		- fonction vérifier la couleur noire
-		- fonction remonter à la case suivante
-	- Système de jeu à 2 joueurs avec currentPlayer = 1 ou 2 
-	- Détection 4 case à la suite de la même couleur 
-*/
-
 var currentPlayer = 1;
 var currentColumn;
 var currentRow = 6;
@@ -16,12 +8,11 @@ var black = 'rgb(0, 0, 0)';
 var yellow = 'rgb(255, 255, 0)';
 var red = 'rgb(255, 0, 0)';
 	
+
 	//Fonction qui renvoie un booléen true si noire, false si colorée
 function verifyColor(row, column){
 	currentColor = $('td[data-row='+row+'][data-column='+column+']').css('background-color');
-	console.log('currentColor: ' + currentColor);
 	if (currentColor === black) {
-		console.log('Test black')
 		return blackOrElse = true;
 	}
 	else {
@@ -31,40 +22,60 @@ function verifyColor(row, column){
 
 	// Fonction qui remonte d'une case vers le haut
 function goUp(row){
-	currentRow = row - 1;
-}
-
-	// Fonction qui teste la couleur de la case et la remplit par la bonne couleur si nécessaire
-function caseColor(row, column){
-	if(blackOrElse === true && currentPlayer === 1){
-		$('td[data-row='+row+'][data-column='+column+']').css('background-color', red);
-		console.log('black donc red');
-		currentPlayer = 2;
-		currentRow = 6;
-	}
-	else if(blackOrElse === true && currentPlayer === 2){
-		$('td[data-row='+row+'][data-column='+column+']').css('background-color', yellow);
-		console.log('black donc jaune')
-		currentPlayer = 1;
-		currentRow = 6;
-	}
-	if(blackOrElse === false){
-		goUp(currentRow);
-		console.log(row);
+	if(currentRow > 1){
+		currentRow = row - 1;	
 	}
 }
 
-function caseColorV2(row, column){
+	// Fonction qui teste la couleur en cour et 
+function caseColor(column){
+	for (var i = 1 ; i <= 6; i++) {
+		verifyColor(currentRow, currentColumn);	
+		if(blackOrElse === true && currentPlayer === 1){
+			console.log(currentRow + ', ' + currentColumn);
+			$('td[data-row='+currentRow+'][data-column='+column+']').css('background-color', red);
+			currentPlayer = 2;
+			break;
+		}
+		else if(blackOrElse === true && currentPlayer === 2){
+			console.log(currentRow + ', ' + currentColumn);
+			$('td[data-row='+currentRow+'][data-column='+column+']').css('background-color', yellow);
+			currentPlayer = 1;
+			break;
+		}
+		if(blackOrElse === false){
+			goUp(currentRow);
+			console.log('UP ' + currentRow + ', ' + currentColumn);
+		}
+	}
+	currentRow = 6;
+}
 
+	// Montre qui est le joueur en cours.
+function colorPlayer(){
+	if(currentPlayer === 1){
+		$('.player1').css('color', red);
+		$('.player1').css('font-size', '22px');
+		$('.player1').css('font-weight', 'bold');
+
+		$('.player2').css('color', black);
+		$('.player2').css('font-size', '17px');
+		$('.player2').css('font-weight', 'normal');
+	}
+	else if(currentPlayer === 2){
+		$('.player2').css('color', yellow);
+		$('.player2').css('font-size', '22px');
+		$('.player2').css('font-weight', 'bold');
+
+		$('.player1').css('color', black);
+		$('.player1').css('font-size', '17px');
+		$('.player1').css('font-weight', 'normal');
+	}
 }
 
 
 $('button').on('click', function(){
 	currentColumn = parseInt($(this).data('column'));
-	console.log(currentRow + ', ' + currentColumn);
-
-	verifyColor(currentRow, currentColumn);
-	caseColor(currentRow, currentColumn);
-	console.log('val currow' + currentRow);
-	//debugger
+	caseColor(currentColumn);
+	colorPlayer();
 });
